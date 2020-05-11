08533ac34d2b99cc31ec36b5908e755897c1e49c
Ugurcan Yildirim <ugurcan.yildirim@accenture.com>
2020-05-11 12:22:07 +0300

    add hot reload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,63 @@
 import {
-    message,
-    getGreeting,
-} from "./mymodule";
+    GraphQLServer
+} from "graphql-yoga";
 
-console.log(message);
-console.log(getGreeting("Ugurcan"));
+//type definitions (schema)
+const typeDefs = `
+    type Query {
+        greeting(name: String): String!
+        grades: [Int!]!
+        me: User!
+        post: Post!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+    }
+`
+
+//resolvers
+const resolvers = {
+    Query: {
+        greeting(parent, args, ctx, info) {
+            if (args.name)
+                return `Hello, ${args.name}!`
+            else
+                return "Hello!"
+        },
+        me() {
+            return {
+                id: "q4214312",
+                name: "Ugurcan",
+                email: "ugurcan@mail.com"
+            };
+        },
+        post() {
+            return {
+                id: "12345",
+                title: "Some post title",
+                body: "Some post body",
+                published: true
+            };
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(() => {
+    console.log("server is up...")
+});
